7266af17f5cad5672cb96871dc8ec06370cf3326
KevinMaganaM <magana.rodrigo31@gmail.com>
2024-08-14 13:56:36 -0700

    products management and configuration

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, offer
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+class offerAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount', 'description')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(offer, offerAdmin)
